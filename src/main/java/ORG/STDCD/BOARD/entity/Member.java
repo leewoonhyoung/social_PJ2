8be252553f2df5a17ff3c85fbadd74af8eb6b2d1
leewoonhyoung <wh3619@gmail.com>
2022-09-09 22:46:29 +0900

    test

---
+++ src/main/java/ORG/STDCD/BOARD/entity/Member.java
@@ -0,0 +1,24 @@
+package ORG.STDCD.BOARD.entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
+public class Member extends BaseEntity{
+
+    @Id @GeneratedValue
+    private String email;
+
+    private String password;
+
+    private String name;
+
+}
